14a50473014dc68de0c874396c9365236173a99a
anonymous <anonymous@users.noreply.github.com>
2026-06-17 14:30:13 +0900

    style: drop redundant quotes in install.sh

--- install.sh
+++ install.sh
@@ -8,6 +8,6 @@ backup_bin=${target_bin}.proprietary.bak
 cd "$current_dir"
 nix develop --accept-flake-config --command zig build
 
-[[ -e "$backup_bin" ]] || sudo cp -a "$target_bin" "$backup_bin"
+[[ -e $backup_bin ]] || sudo cp -a "$target_bin" "$backup_bin"
 sudo pkill -f "(^|/)gpservice( |$)|(^|/)gpgui( |$)" 2>/dev/null || true
 sudo install -m 755 "$current_dir/zig-out/bin/gpgui-free" "$target_bin"
